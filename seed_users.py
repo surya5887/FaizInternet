e8d5be93fe2096ba7cffe208206e3c00efb94f22
Anees Chaudhary <samirchaudhary8558@gmail.com>
2026-02-21 20:43:44 +0530

    updated

--- seed_users.py
+++ seed_users.py
@@ -41,8 +41,7 @@ def seed_users():
                 name="Anees Chaudhary",
                 email=super_email,
                 phone="",
-                password=generate_password_hash(super_pass),
-                plain_password=super_pass,
+                password=super_pass,
                 role="superuser"
             )
             db.session.add(superuser)
