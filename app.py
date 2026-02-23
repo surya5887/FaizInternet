1934afb332eb817db5b14877ec20e2fdf47a0a46
Anees Chaudhary <samirchaudhary8558@gmail.com>
2026-02-23 20:43:53 +0530

    Final Polish: standardized phone number in seed data and removed stale comments in admin templates

--- app.py
+++ app.py
@@ -341,7 +341,7 @@ def init_db():
                 'shop_name': 'Faiz Internet',
                 'shop_tagline': 'Common Service Centre',
                 'shop_address': 'Maiz Bazar, Asara, Uttar Pradesh, India',
-                'shop_phone': '+91 9837957711',
+                'shop_phone': '+91 9756520529',
                 'shop_email': 'contact@faizinternet.com',
                 'shop_timings': 'Mon - Sat: 9AM - 8PM',
                 'shop_map_url': 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3510.123456789!2d78.0!3d28.5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjjCsDMwJzAwLjAiTiA3OMKwMDAnMDAuMCJF!5e0!3m2!1sen!2sin!4v1234567890'
